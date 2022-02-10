8f7b98457df247e0de6d1d6a73290c965e9647a9
Jack Jones <jackij43@outlook.com>
2022-02-10 16:20:32 -0600

    added setTeam and override in red version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarouselSideAutonomusRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CarouselSideAutonomusRed.java
@@ -29,72 +29,10 @@ import org.firstinspires.ftc.teamcode.lib.OpModeBasics;
 
 public class CarouselSideAutonomusRed extends CarosuelSideAutonomus{
 
-    // todo: write your code here
+    //set team to red team
     @Override
-    public void init() {
-        front_right = hardwareMap.dcMotor.get("front_right");
-        front_left = hardwareMap.dcMotor.get("front_left");
-        back_right = hardwareMap.dcMotor.get("back_right");
-        back_left = hardwareMap.dcMotor.get("back_left");
-
-        front_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        front_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        back_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        back_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        front_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        front_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        back_right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        back_left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        front_right.setDirection(DcMotorSimple.Direction.REVERSE);
-        back_right.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        carouselMotor = hardwareMap.dcMotor.get("CarouselMotor");
-        liftMotor = hardwareMap.dcMotor.get("LiftMotor");
-
-        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        distR = hardwareMap.get(DistanceSensor.class, "DistR");
-        distL = hardwareMap.get(DistanceSensor.class, "DistL");
-        distBack = hardwareMap.get(DistanceSensor.class, "DistBack");
-
-        left_intake = hardwareMap.get(Servo.class, "left_intake");
-        right_intake = hardwareMap.get(Servo.class, "right_intake");
-
-        right_intake.setDirection(Servo.Direction.REVERSE);
-
-        vuforiaFreightFrenzy = new VuforiaCurrentGame();
-        tfodFreightFrenzy = new TfodCurrentGame();
-
-        vuforiaFreightFrenzy.initialize(
-                "", //Vuforia Liscense Key
-                hardwareMap.get(WebcamName.class, "Webcam 1"), //Webcam Name
-                "", //Webcam Calibration Filename
-                false, //Use Extended Tracking
-                true, //Enable Camera Monitoring
-                VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES, //Camera Monitor Feedback
-                0, //dx
-                0, //dy
-                0, //dz
-                AxesOrder.XYZ, //Axes Order
-                90, //First Angle
-                90, //Second Angle
-                0, //Third Angle
-                true //Use competition feild target locations
-        );
-
-        tfodFreightFrenzy.initialize(
-                vuforiaFreightFrenzy, //Vuforia
-                (float) 0.5, //Minimum confidence
-                true, //Use object tracker
-                true //Enable camera monitoring
-        );
-
-        tfodFreightFrenzy.activate();
-        tfodFreightFrenzy.setZoom(1, 16 / 9);
-
-        basics = new OpModeBasics(front_right, front_left, back_right, back_left);
+    public void setTeam() {
         blueTeam = false;
     }
+
 }
